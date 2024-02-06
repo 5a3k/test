3197d0e43558222fa1148e2579624eb6e7c91b87
5a3k <140529270+5a3k@users.noreply.github.com>
2024-02-06 00:26:57 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,30 @@
 local bypass = {
-    -- Your character replacement mapping
+    ["a"] = "а",
+    ["b"] = "b",
+    ["c"] = "с",
+    ["d"] = "d",
+    ["e"] = "е",
+    ["f"] = "f",
+    ["g"] = "g",
+    ["h"] = "һ",
+    ["i"] = "і",
+    ["j"] = "ј",
+    ["k"] = "k",
+    ["l"] = "ӏ",
+    ["m"] = "m",
+    ["n"] = "n",
+    ["o"] = "о",
+    ["p"] = "р",
+    ["q"] = "q",
+    ["r"] = "r",
+    ["s"] = "ѕ",
+    ["t"] = "t",
+    ["u"] = "u",
+    ["v"] = "v",
+    ["w"] = "w",
+    ["x"] = "х",
+    ["y"] = "у",
+    ["z"] = "z",
 }
 
 local TCS = game:GetService("TextChatService")
@@ -8,7 +33,7 @@ local Channel = TCS.TextChannels.RBXGeneral
 
 local function gen(txt)
     local new = ""
-    for _, letter in next, txt:split("") do
+    for _,letter in next, txt:split("") do
         if bypass[letter] then
             new = new .. bypass[letter]
         else
@@ -22,20 +47,18 @@ local Old = nil
 TCS.OnIncomingMessage = function(msg)
     local Owner = tostring(msg.TextSource)
     local Prop = Instance.new("TextChatMessageProperties")
-
-    if Owner == LocalPlayer.Name and not Old then
+    if Owner == LocalPlayer.Name then
         Old = gen(msg.Text)
         Prop.Text = " "
     else
         Prop.Text = msg.Text
     end
-
     return Prop
 end
 
 TCS.MessageReceived:Connect(function(msg)
     local Owner = tostring(msg.TextSource)
-    if Owner == LocalPlayer.Name and Old then
+    if Owner == LocalPlayer.Name and not Old then
         Channel:SendAsync(Old)
         Old = nil
     end
